acc74361fe7071a759d7c2d84f97a01f04a4cac7
Kyle Bailey <bailey.k.r.12@gmail.com>
2025-07-10 12:44:03 +0200

    Add dynamic product detail page

---
+++ src/js/ProductDetails.mjs
@@ -0,0 +1,31 @@
+import { setLocalStorage } from './utils.mjs';
+
+export default class ProductDetails {
+  constructor(productId, dataSource) {
+    this.productId = productId;
+    this.product = {};
+    this.dataSource = dataSource;
+  }
+
+  async init() {
+    this.product = await this.dataSource.findProductById(this.productId);
+    this.renderProductDetails();
+
+    document.getElementById('addToCart')
+      .addEventListener('click', this.addProductToCart.bind(this));
+  }
+
+  addProductToCart() {
+    setLocalStorage('so-cart', this.product);
+  }
+
+  renderProductDetails() {
+    document.querySelector('.product-detail').innerHTML = `
+      <h3>${this.product.Name}</h3>
+      <img src="${this.product.Image}" alt="${this.product.Name}">
+      <p class="price">$${this.product.FinalPrice}</p>
+      <p>${this.product.Description}</p>
+      <button id="addToCart">Add to Cart</button>
+    `;
+  }
+}
--- src/js/product.js
+++ src/js/product.js
@@ -1,18 +1,34 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getParam, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+
+// Get product ID from query string
+const productId = getParam("product");
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
   setLocalStorage("so-cart", product);
 }
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
+
+async function renderProductDetails(productId) {
+  const product = await dataSource.findProductById(productId);
+
+  document.querySelector(".product-detail h3").textContent = product.Brand.Name;
+  document.querySelector(".product-detail h2").textContent = product.NameWithoutBrand;
+  document.querySelector(".product-detail img").src = product.Images.Primary.Medium;
+  document.querySelector(".product-detail img").alt = product.Name;
+  document.querySelector(".product-card__price").textContent = `$${product.ListPrice}`;
+  document.querySelector(".product__color").textContent = product.Colors[0].ColorName;
+  document.querySelector(".product__description").textContent = product.DescriptionHtmlSimple;
+
+  document.getElementById("addToCart").dataset.id = product.Id;
 }
 
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+renderProductDetails(productId);
+
+document.getElementById("addToCart").addEventListener("click", async function (e) {
+  const product = await dataSource.findProductById(e.target.dataset.id);
+  addProductToCart(product);
+});
+
+
